5d64623946a887e8e90aba258fd80299aa8de346
Andrew <andrew.jpbarbosa@gmail.com>
2023-04-20 20:34:15 -0300

    v1

---
+++ src/pages/component/ass/index.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @next/next/no-img-element */
+import { toJpeg } from "html-to-image";
+import { saveAs } from "file-saver";
+import Image from "next/image";
+
+interface assProps {
+  nome?: string;
+  ramal?: string;
+}
+
+export function Ass({ nome, ramal }: assProps) {
+  const downloadComponentAsJPG = async (elementId: any, fileName: any) => {
+    const element = document.getElementById(elementId);
+    if (!element) return;
+
+    try {
+      const dataUrl = await toJpeg(element);
+      saveAs(dataUrl, fileName);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <div
+        id="my-ass"
+        className="flex flex-row bg-gray-50 rounded-md items-center "
+      >
+        <div className="flex">
+          <img
+            src="/logo.jpeg"
+            alt="logo"
+            className="rounded-md text-center items-center w-[110px] "
+          />
+          <div className="h-[70px] w-[2px] bg-[#626466]/50 items-center m-auto text-transparent ml-2">
+            |
+          </div>
+        </div>
+        <div className="text-gray-800 p-4">
+          <p className="text-[#c61d37] mb-2">
+            {nome ? nome : "Nome e Sobrenome"}
+          </p>
+          <div className="text-[#626466]">
+            <p>11 2122 0212 | Ramal {ramal ? ramal : "0000"}</p>
+            <p>www.spotpromo.com.br</p>
+          </div>
+        </div>
+      </div>
+      <button
+        disabled={ramal?.length === 0 || nome?.length === 0 ? true : false}
+        className={`my-4 w-full bg-[#c61d37] p-2 rounded-md  disabled:bg-gray-700 cursor-pointer disabled:cursor-not-allowed`}
+        onClick={() => downloadComponentAsJPG("my-ass", `assinatura-${nome}`)}
+      >
+        Baixar Assinatura
+      </button>
+    </div>
+  );
+}
---
+++ src/pages/component/input/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { IconType } from "react-icons";
+
+interface inputProps {
+  placeholder: string;
+  setValue: (value: string) => void;
+  value: string;
+  Icon: IconType;
+}
+
+export function Input({ placeholder, setValue, value, Icon }: inputProps) {
+  const [focus, setFocus] = useState(false);
+  return (
+    <div
+      className={`p-2 rounded-md w-full my-2 text-gray-200 bg-gray-900 border flex items-center gap-2 ${
+        focus ? "border-[#c61d37]" : "border-transparent"
+      }`}
+    >
+      <Icon
+        className={`${focus || value.length > 0 ? "text-green-800" : ""}`}
+      />
+      <input
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+        placeholder={placeholder}
+        className="bg-transparent w-80 focus:outline-none"
+      />
+    </div>
+  );
+}
